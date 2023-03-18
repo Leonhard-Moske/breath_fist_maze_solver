a1b8ce411b570362a05f16db800149d18e5ebc9c
Leonhard-Moske <leonhard.moske@gmail.com>
2023-03-18 14:03:23 +0100

    end for now

--- dist/breath_first_maze_solver-a13e4f63ae9a3800.js
+++ dist/breath_first_maze_solver-a13e4f63ae9a3800.js
@@ -581,11 +581,11 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper3738 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper3739 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 235, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3865 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper3866 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 256, __wbg_adapter_19);
         return addHeapObject(ret);
     };
@@ -624,7 +624,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('breath_first_maze_solver-ed75380cead69e03_bg.wasm', import.meta.url);
+        input = new URL('breath_first_maze_solver-a13e4f63ae9a3800_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,47 @@ fn App() -> Html {
         }
     };
 
+    let mut maze = maze::maze_mod::Maze {
+        vertical_weight: 0.6,
+        ..Default::default()
+    };
+
+    //have a button to generate maze
+
+    
+    
+    //have a button to solve maze
+
+    //have a button to reset maze
+
+    //have a button to set start point
+
+    //have a button to set end point
+
+
+
+
+
+
     html! {
         <div>
             <button {onclick}>{ "+1" }</button>
             <p>{ *counter }</p>
+            <canvas>
+            </canvas>
+            <div>
+            // button to set start point
+            
+            // button to set end point
+
+            // button to generate maze
+            // button to solve maze
+            // button to reset maze
+
+
+
+
+            </div>
         </div>
     }
 }
